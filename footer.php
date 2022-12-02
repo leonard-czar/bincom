2257bee1f7c0729129cd57ce6eb0f332cd4aa8b2
leonard lebechi <leonell4fame@gmail.com>
2022-12-02 12:34:10 +0100

    bootsrap

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<script src="bootstrap/bootstrap/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <footer style="justify-content:space-between;background-color: #050C24;">
     <div class="container-fluid-sm">
         <div class="row">
